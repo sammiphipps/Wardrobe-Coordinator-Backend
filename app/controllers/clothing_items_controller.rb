4c3766a8efe949735df338755d8a69ac857cc3aa
Samantha Phipps <sammi.phipps@gmail.com>
2020-01-15 17:57:18 -0700

    created index_by_users method which will allow me to retrieve all of the clothing items associated with a specific user when the username is sent in the params. Needed to modify the routes to include this specific route. When tested in postman is currently working correctly. I did this so that on the front end I can send a fetch to the backend when a user logins that will only retrieve their information.

--- app/controllers/clothing_items_controller.rb
+++ app/controllers/clothing_items_controller.rb
@@ -15,6 +15,12 @@ before_action :authenticate, only: [:create, :update, :delete]
         render json: clothing_item, except: [:clothing_category_id], include: [:clothing_category => {only: [:name]}]
     end 
 
+    def index_by_users
+        user = User.find_by(username: params[:username])
+        clothing_items = ClothingItem.where(user_id: user.id)
+        render json: clothing_items, except: [:clothing_category_id], include: [:clothing_category => {only: [:name]}]
+    end 
+
     def create 
         clothing_item = ClothingItem.create(clothing_item_params)
         render json: clothing_item, except: [:clothing_category_id], include: [:clothing_category => {only: [:name]}]
