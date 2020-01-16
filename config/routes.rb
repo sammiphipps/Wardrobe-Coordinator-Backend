f6f3ba5d09a9f9817e3ae67a1f7dd752913c9bf3
Samantha Phipps <sammi.phipps@gmail.com>
2020-01-16 15:34:41 -0700

    Removed token_check from authentication controller and the authenticate route as I don't actually need it. Instead, I can run the authenticate method before doing the index by user method in the clothing items controller to get the user name from the token. Due to this I renamed the the route to user_clothing_items as I don't need any params and I don't want to confuse any of the standard clothing item routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post "login", to: "authentication#login"
-  get "/authenticate", to: "authentication#token_check"
-  get "/clothing_items/user/:username", to: "clothing_items#index_by_user"
+  get "/user_clothing_items", to: "clothing_items#index_by_user"
 
   resources :clothing_items, except: [:new, :edit]
   resources :clothing_categories, only: [:index, :show]
