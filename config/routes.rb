ab65a5f87957ed00ffb647eb875baa0495a6ef2c
Samantha Phipps <sammi.phipps@gmail.com>
2020-01-16 12:59:53 -0700

    Added an error message for if the user is unauthorized and added user_id and username to the return if authorized. I added this to be able to do post and ect on the frontend, but am reconsidering. I may up changing this again. Also, changed route to go to the index by user method on the clothing item controller. I did this because I found that when I tried to send a post to clothing items with an id, it was going to this method. By adding something in between, I can still get the information with the index and be able to send post, put and delete.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post "login", to: "authentication#login"
-  get "/clothing_items/:username", to: "clothing_items#index_by_user"
+  get "/clothing_items/user/:username", to: "clothing_items#index_by_user"
 
   resources :clothing_items, except: [:new, :edit]
   resources :clothing_categories, only: [:index, :show]
