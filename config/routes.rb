609fb12b647522c81f3af1a4cccd556d37cd848f
Samantha Phipps <sammi.phipps@gmail.com>
2020-01-21 17:36:05 -0700

    Added create_with_multiple_items method to outfit items so that I can send multiple items in one fetch.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post "login", to: "authentication#login"
   get "/user_clothing_items", to: "clothing_items#index_by_user"
   get "/user_outfits", to: "outfits#index_by_user"
+  post "/multiple_outfit_items", to: "outfit_items#create_with_multiple_items"
 
   resources :clothing_items, except: [:new, :edit]
   resources :clothing_categories, only: [:index, :show]
